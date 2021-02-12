5772cb08494d9b186f54d88a70e421cf1cd94cf7
Allians <allians19@yandex.ru>
2021-02-12 11:00:07 +0500

    Домашка 1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const numbersOfFilms = +prompt('Сколько Вильмов вы уже посмотрели?', '5');
+
+
+const personalMovieDB = {
+	count: numbersOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', ''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
